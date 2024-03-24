5163d491b99ccf252c40b727cd9b74e02c3ea70c
bijeshofficial <bijeshr.kunwar@gmail.com>
2024-03-25 01:42:28 +0545

    build file and vercel change

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 echo "BUILD START"
-python3.10  -m pip3.10 install -r requirements.txt
+python3.9  -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-python3.10 manage.py collectstatic  --noinput --clear
+python3.9 manage.py collectstatic  --noinput --clear
 echo "BUILD END"
